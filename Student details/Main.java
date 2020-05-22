78f189df4f29f9675b2c6ca03ae0ab411f021d22
Ritwick_Bhaduri <ritwickbhaduri7@gmail.com>
2020-05-23 02:00:30 +0530

    Update Main.java

--- Student details/Main.java
+++ Student details/Main.java
@@ -13,7 +13,9 @@ int main()
   int n;
   cout<<"Enter the number of students";
   cin>>n;
-  struct Student s[10]
+  struct Student s[10],t;
+  for(int i=0;i<n;i++)
+  {
     cout<<"\nEnter the details of student "<<i+1;
     cout<<"\nEnter name";
     cin>>s[i].name;
@@ -23,10 +25,26 @@ int main()
     cin>>s[i].yearOfStudy;
     cout<<"\nEnter cgpa";
     cin>>s[i].cgpa;
+  }
+  for(int i=1; i<n; i++)
+  {
+    for(int j=1; j<n; j++)
+    {
+      if(strcmp(s[j-1].name, s[j].name)>0)
+      {
+        t=s[j-1];
+        s[j-1]=s[j];
+        s[j]=t;
+      }
+    }
+  }
   cout<<"\nDetails of students";
+  for(int i=0;i<n;i++)
+  {
     cout<<"\nStudent "<<i+1;
     cout<<"\nName:"<<s[i].name;
     cout<<"\nDepartment:"<<s[i].department;
     cout<<"\nYear of study:"<<s[i].yearOfStudy;
     cout<<"\nCGPA:"<<s[i].cgpa;
+  }
 }
